ecb36a26bd484ce221efabcc8740e5e6f8cb7e25
Ryan Cybul <ryan.cybul@gmail.com>
2018-03-22 20:16:07 -0500

    Added a button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
 
-    let key = 'Oqf2S0Ti12Rvjts34nggsvkIMbf6WLFg';   
-    let queryURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=Oqf2S0Ti12Rvjts34nggsvkIMbf6WLFg&limit=5";
+    //Variables
+    let buttonArray = ['Homer', 'Bart', 'Marge', 'Lisa', 'Millhouse', 'Smithers', 'Mr. Burns', 'Barney'];
+  
+    //Functions
+    function init() {
+        printButtons();
+    }
+
+    function printButtons() {
+        //Emptys the array
+        $('.js-buttons').empty();
+        for (let i = 0; i < buttonArray.length; i++) {
+            var newButton = $('<button type="button" value="' + buttonArray[i]+ '">' + buttonArray[i] + '</button>');
+            $('.js-buttons').append(newButton);
+        }
+    }
+
+   let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  + "&api_key=Oqf2S0Ti12Rvjts34nggsvkIMbf6WLFg&limit=5";
 
     $.ajax ({
-        URL: queryURL,
+        url: queryURL,
         method: "GET"
     }).then(function(response) {
     console.log(response);
     });
+
+    //Events
+    //Sets the initial state on page load
+    init();
+
+    //Submits user input to array and generates button
+    $('.submitButton').on('click', function(event) {
+        //Preventing the button from trying to submit the form
+        event.preventDefault();
+        //Stores the input in a variable
+        var newChar = $('.js-text-input').val().trim();
+        //Pushes new character to Variables
+        buttonArray.push(newChar);
+        //Clears Text Input
+        $('.js-text-input').val('');
+        printButtons();
+    });
+
+    $()
 
 });
