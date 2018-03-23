64c62da536771f674ab1bad46378ac919c1d8e43
Ryan Cybul <ryan.cybul@gmail.com>
2018-03-22 22:23:49 -0500

    The dang new button wont work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
     //Variables
-    let buttonArray = ['Homer', 'Bart', 'Marge', 'Lisa', 'Millhouse', 'Smithers', 'Mr. Burns', 'Barney'];
-  
+    let buttonArray = ['Homer Simpson', 'Bart Simpson', 'Marge Simpson', 'Lisa Simpson', 'Millhouse', 'Smithers', 'Mr Burns', 'Barney Gumble'];
+
     //Functions
     function init() {
         printButtons();
@@ -11,20 +11,13 @@ $(document).ready(function() {
     function printButtons() {
         //Emptys the array
         $('.js-buttons').empty();
+        //Prints array
         for (let i = 0; i < buttonArray.length; i++) {
-            var newButton = $('<button type="button" value="' + buttonArray[i]+ '">' + buttonArray[i] + '</button>');
+            let newButton = $('<button type="button" value="' + buttonArray[i]+ '">' + buttonArray[i] + '</button>');
+            newButton.addClass('button');
             $('.js-buttons').append(newButton);
         }
     }
-
-   let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  + "&api_key=Oqf2S0Ti12Rvjts34nggsvkIMbf6WLFg&limit=5";
-
-    $.ajax ({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    });
 
     //Events
     //Sets the initial state on page load
@@ -43,6 +36,26 @@ $(document).ready(function() {
         printButtons();
     });
 
-    $()
+    //Button on-click event
+    $('.button').on('click', function printGifs() {
+        //Empty display div
+        $('.js-gifs').empty();
+
+        //Pulls name from character clicked
+        let charClicked = this.value;
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + charClicked + "&api_key=Oqf2S0Ti12Rvjts34nggsvkIMbf6WLFg&limit=10";
+        
+        $.ajax ({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+        for (let i = 0; i < response.data.length; i++) {
+            let url = response.data[i].images.original.url;
+            let charGif = $('<img>');
+            charGif.attr('src', url);
+            $('.js-gifs').prepend(charGif);
+        }
+    });
+    });
 
 });
